63f5d655c1b3baab76a881d7e093a0d22a66c921
OliverCode-maker <olinavar@espol.edu.ec>
2025-07-06 16:05:59 -0500

    Actualizacion clase Sistema (#13)
    * feat:He creado clases basicas
    -He creado la clase Pedido
    -He creado la clase Producto
    
    * feat():Actualización de Clase Pedido
     -He creado los atributos de la clase Pedido
     -He creado el constructor de la clase Pedido
     -He creado los metodos getter y setters respectivos
    
    * Actualizacion clase sistema
    
    ---------

--- src/main/java/proyecto/Sistema.java
+++ src/main/java/proyecto/Sistema.java
@@ -1,7 +1,77 @@
 package proyecto;
+import java.util.Scanner;
+import java.util.ArrayList;
+import proyecto.usuario.*;
 
 public class Sistema {
+    private ArrayList<Usuario> usuarios;
+
+    public Usuario IniciarSesion(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("============INICIAR SESIÓN============");
+        System.out.println("Usuario: ");
+        String usuario= sc.nextLine();
+        System.out.println("Contraseña: ");
+        String contrasenia= sc.nextLine();
+
+        for (Usuario user : this.usuarios) {
+            if(user.getUserName().equals(usuario) && user.getContrasena().equals(contrasenia)){
+                return user;
+            }
+        }
+        return null;
+    }
+    public boolean validarUsuario(Usuario user){
+        Scanner sc= new Scanner(System.in);
+        if (user==null){
+            System.out.println("Error al autenticar usuario");
+            return false;
+        }else{
+            System.out.println("Usuario autenticado exitosamente");
+            char rol= user.getCodigoUnico().charAt(0);
+            if (rol=='C'){
+                Cliente c= (Cliente)user;
+                System.out.println("Rol detectado: CLIENTE");
+                System.out.println("Bienvenid@ "+user.getNombres()+" "+user.getApellidos());
+                System.out.println("Celular registrado: "+c.getCelular());
+                System.out.println("¿Este número celular es correcto? (S/N): ");
+            }else{
+                Repartidor r= (Repartidor)user;
+                System.out.println("Rol detectado: REPARTIDOR");
+                System.out.println("Bienvenid@ "+user.getNombres()+" "+user.getApellidos());
+                System.out.println("Empresa asginada: "+r.getEmpresa());
+                System.out.println("¿Esta empresa es correcta? (S/N)");
+            }
+            String sn= sc.nextLine();
+            if(sn.equals("S")){
+                System.out.println("Identidad Confirmada");
+                return true;
+            }else{
+                System.out.println("Verificación fallida");
+                System.out.println("Por motivos de seguridad se cerrará sesión");
+                System.out.println("Saliendo del sistema...");
+                return false;
+            }
+
+        }
+    }
+    public void mostrarmenu(Usuario user, boolean verificacion){
+        if(user instanceof Cliente){
+            Cliente c= (Cliente)user;
+            System.out.println("Menú de Cliente");
+            System.out.println("1. Comprar producto\n 2. Gestionar Pedido\n 3. Salir\n Seleccione una opción: ");
+        }else{
+            Repartidor r= (Repartidor)user;
+            System.out.println("Menú de Repartidor");
+            System.out.println("1. Consultar pedidos\n 2. Gestionar Pedido\n 3. Salir\n Seleccione una opción: ");
+
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hola soy Pablo");
+        Sistema sis = new Sistema();
+        Usuario user=sis.IniciarSesion();
+        boolean val =sis.validarUsuario(user);
+        
+        sis.mostrarmenu(user, val);
     }
 }
--- src/main/java/proyecto/pedido/Pedido.java
+++ src/main/java/proyecto/pedido/Pedido.java
@@ -1,5 +1,62 @@
+import proyecto.usuario.Repartidor;
 package proyecto.pedido;
 
-public class Pedido {
 
+public class Pedido {
+    private String codigoPedido;
+    private String fechaPedido;
+    private int cantPedido;
+    private double valorPagado;
+    private EstadoPedido estado;
+    private Repartidor repartidor;
+
+    //Contructor de Clase
+    public Pedido(String codigoPedido, String fechaPedido, int cantPedido, double valorPagado, EstadoPedido estado, Repartidor repartidor){
+        this.codigoPedido=codigoPedido;
+        this.fechaPedido=fechaPedido;
+        this.cantPedido=cantPedido;
+        this.valorPagado=valorPagado;
+        this.estado=estado;
+        this.repartidor=repartidor;
+    }
+
+    //Setters
+    public void setCantPedido(int cantPedido) {
+        this.cantPedido = cantPedido;
+    }
+    public void setCodigoPedido(String codigoPedido) {
+        this.codigoPedido = codigoPedido;
+    }
+    public void setEstado(EstadoPedido estado) {
+        this.estado = estado;
+    }
+    public void setFechaPedido(String fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+    public void setRepartidor(Repartidor repartidor) {
+        this.repartidor = repartidor;
+    }
+    public void setValorPagado(double valorPagado) {
+        this.valorPagado = valorPagado;
+    }
+
+    //Getters
+    public int getCantPedido() {
+        return cantPedido;
+    }
+    public String getCodigoPedido() {
+        return codigoPedido;
+    }
+    public EstadoPedido getEstado() {
+        return estado;
+    }
+    public String getFechaPedido() {
+        return fechaPedido;
+    }
+    public Repartidor getRepartidor() {
+        return repartidor;
+    }
+    public double getValorPagado() {
+        return valorPagado;
+    }
 }
